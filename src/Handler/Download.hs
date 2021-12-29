4f7002c646a2db89ed275e37114c3334f2ee29c1
Azizul Karim <gebach@Azizuls-Mac-mini.local>
2021-12-30 04:17:34 +0600

    fixed lot of things but renaming still a concern

--- src/Handler/Download.hs
+++ src/Handler/Download.hs
@@ -1,11 +1,25 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Handler.Download where
 
 import Import
+import System.Directory
 
-getDownloadR :: String -> Handler Html
+getDownloadR :: String -> Handler ()
 getDownloadR filename = do
   let path_ = generateFilePath filename
-  sendFile typeOctet path_
+  _ <- sendFile typeOctet path_
+  maybeParam <- lookupGetParam "id"
+  case maybeParam of
+    Just param -> do
+      let newPath = generateFilePath $ unpack param
+      liftIO $ renamePath path_ newPath
+    Nothing -> return ()
 
 generateFilePath :: String -> FilePath
 generateFilePath f = uploadDirectory </> f
--- src/Handler/Fetch.hs
+++ src/Handler/Fetch.hs
@@ -3,22 +3,27 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Handler.Fetch where
 
 import Import
 import System.Directory
 
-
 getFetchR :: Handler ()
 getFetchR = do
   maybeParam <- lookupGetParam "id"
   case maybeParam of
     Just param -> do
-      let oldPath = generateFilePath $ unpack param
-          newPath =  generateFilePath "hello.txt"
-      liftIO $ renamePath oldPath newPath
-      redirect (("/download/" <> "hello.txt") :: String)
+      maybeFile <- runDB $ getFilename $ unpack param
+      case maybeFile of
+        Just file -> do
+          let oldPath = generateFilePath $ unpack param
+              newPath =  generateFilePath filename_
+              filename_ = fileFilename $ entityVal file
+          liftIO $ renamePath oldPath newPath
+          redirect (("/download/" <> filename_ <> "?id=" <> (unpack param)) :: String)
+        Nothing -> return ()
     Nothing -> return ()
 
 generateFilePath :: String -> FilePath
@@ -26,6 +31,10 @@ generateFilePath f = uploadDirectory </> f
 
 uploadDirectory :: FilePath
 uploadDirectory = "shared_files"
+
+getFilename :: String -> DB (Maybe (Entity File))
+getFilename uuid =
+  selectFirst [FileUuid ==. uuid ] []
 
 {--
 getFetchR :: String -> Handler Html
