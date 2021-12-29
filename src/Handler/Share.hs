dc051f4bd31dc34d8c8356d64e6e424a3a24fcbd
Azizul Karim <gebach@Azizuls-Mac-mini.local>
2021-12-29 19:25:53 +0600

    added some utility functions to Share.hs

--- src/Handler/Share.hs
+++ src/Handler/Share.hs
@@ -7,6 +7,13 @@
 module Handler.Share where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Text.Julius (RawJS (..))
+import Database.Persist
+import Database.Persist.Sqlite
+import Data.Time (getCurrentTime, addUTCTime, secondsToNominalDiffTime) 
+import Data.Fixed
+import Data.UUID (toString)
+import Data.UUID.V4 (nextRandom)
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
@@ -21,18 +28,26 @@ getShareR = do
         setTitle "Share a file"
         $(widgetFile "share")
 
+addTime :: Pico -> UTCTime -> UTCTime
+addTime min utct =
+  addUTCTime (secondsToNominalDiffTime (min * 60)) utct
+
+utctToString :: UTCTime -> String
+utctToString utct =
+  formatTime defaultTimeLocale "%H:%M" utct
+
 sampleForm :: Form FileForm
 sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
-    <$> fileAFormReq "Choose a file"
+    <$> fileAFormReq "Choose a file to share"
     <*> areq textField textSettings Nothing
     -- Add attributes like the placeholder and CSS classes.
     where textSettings = FieldSettings
-            { fsLabel = "What's on the file?"
+            { fsLabel = "Name the file"
             , fsTooltip = Nothing
             , fsId = Nothing
             , fsName = Nothing
             , fsAttrs =
                 [ ("class", "form-control")
-                , ("placeholder", "File description")
+                , ("placeholder", "File name")
                 ]
             }
